4710d65a4c225daf4f66391f141dbe51cea8a113
somnath-more <somnath.more19@vit.edu>
2024-10-31 14:31:28 +0530

    Added database ip and each microservice ip config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ const App = () => {
 
   return (
     <div>
+      <p>This is a Minet application. - {process.env.name}</p>
       <Routes>
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/login" element={<SignInPage />} />
